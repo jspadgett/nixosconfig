b9d382bd8aea90702c69480d439825b7434962bf
Iciera <jshpadgett2@gmail.com>
2024-11-21 12:32:38 -0500

    added ardour

--- configuration.nix
+++ configuration.nix
@@ -62,13 +62,9 @@
   #Enable unfree packages  
     nixpkgs.config.allowUnfree = true;
   # Enable vm manager
-    virtualisation.libvirtd.enable = true;
-    programs.virt-manager.enable = true; 
+    virtualisation.libvirtd.enable = true; programs.virt-manager.enable = true;
   # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
+  sound.enable = true; hardware.pulseaudio.enable = false; security.rtkit.enable = true; services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
@@ -139,6 +135,7 @@ services.flatpak.enable = true;
     wineWowPackages.waylandFull
     calf 
     bottles
+    ardour
     ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -165,13 +162,11 @@ services.flatpak.enable = true;
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
    networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # this should add calf synth to thepath correctly
+  
+  # This value determines the NixOS release from which the default settings for stateful data, like file locations and database versions on your system 
+  # were taken. It‘s perfectly fine and recommended to leave this value at the release version of the first install of this system. Before changing this 
+  # value read the documentation for this option (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
 }
