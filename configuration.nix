9a4f52e289f319d4debcc46a20d8cda7e4b7e71e
Iciera <jshpadgett2@gmail.com>
2024-11-21 19:09:55 -0500

    added htop

--- configuration.nix
+++ configuration.nix
@@ -125,6 +125,7 @@ services.flatpak.enable = true;
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    htop
     wget
     virt-manager
     obs-studio
