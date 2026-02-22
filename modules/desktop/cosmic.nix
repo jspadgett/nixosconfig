a4e6ed436adbc876cc3d3414692ca687f64c59c0
Iciera <jshpadgett2@gmail.com>
2026-02-22 00:54:01 -0500

    added system76 scheduler to cosmic.nix

--- modules/desktop/cosmic.nix
+++ modules/desktop/cosmic.nix
@@ -3,6 +3,6 @@
    services.xserver.enable = true;
    services.displayManager.cosmic-greeter.enable = true;
    services.desktopManager.cosmic.enable = true;
-  
+   services.system76-scheduler.enable = true;  
   }
 
