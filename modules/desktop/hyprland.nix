364e69d95210d3e385154bf2d5ed3b4df1137504
Iciera <jshpadgett2@gmail.com>
2026-02-22 09:36:51 -0500

    adjusted for misspelling

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -39,7 +39,7 @@
   xdg.portal = {
     enable = true;
     extraPortals = [ 
-      pkgs.xdg.desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal-gtk
   ];
     config.hyprland.default = [ "hyprland" "gtk" ];
